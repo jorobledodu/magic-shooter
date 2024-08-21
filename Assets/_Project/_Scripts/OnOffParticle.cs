8d074f9f68c971af37d9dd42be0cf4e61dd70528
FakeGeorge <105489725+FakeGeorge@users.noreply.github.com>
2024-08-21 12:43:12 +0200

    update

--- Assets/_Project/_Scripts/OnOffParticle.cs
+++ Assets/_Project/_Scripts/OnOffParticle.cs
@@ -9,12 +9,14 @@ public class OnOffParticle : MonoBehaviour
     private CFXR_Effect thisEfectosParticle;
 
     public bool isOn;
+    public GameObject icon;
 
     // Start is called before the first frame update
     void Start()
     {
         thisParticleSystem = GetComponent<ParticleSystem>();
         thisEfectosParticle = GetComponent<CFXR_Effect>();
+        icon = this.transform.GetChild(2).gameObject;
 
         if (this.CompareTag("Obstaculo/Linterna"))
         {
@@ -37,12 +39,13 @@ public class OnOffParticle : MonoBehaviour
 
         if (this.CompareTag("Obstaculo/Linterna"))
         {
+            icon.SetActive(false);
             GameManager.instance.MasTiempo();
         }
         else if (this.CompareTag("Obstaculo/Fuego"))
         {
+            icon.SetActive(true);
             this.GetComponent<Collider>().isTrigger = false;
-
             return;
         }
     }
@@ -56,10 +59,12 @@ public class OnOffParticle : MonoBehaviour
 
         if (this.CompareTag("Obstaculo/Linterna"))
         {
+            icon.SetActive(true);
             return;
         }
         else if (this.CompareTag("Obstaculo/Fuego"))
         {
+            icon.SetActive(false);
             GameManager.instance.MasTiempo();
             this.GetComponent<Collider>().isTrigger = true;
         }
