6fa02bd3dcc477df807521131de806512612fef8
FakeGeorge <105489725+FakeGeorge@users.noreply.github.com>
2024-08-21 09:53:07 +0200

    update

--- Assets/_Project/_Scripts/FaceCamera.cs
+++ Assets/_Project/_Scripts/FaceCamera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
+    private Transform cameraTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.rotation = Quaternion.LookRotation(this.transform.position - Camera.main.transform.position);
+        transform.LookAt(cameraTransform);
+        this.transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
     }
 }
